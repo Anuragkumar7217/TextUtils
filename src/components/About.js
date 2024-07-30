59036227467a9d972fbb9460790dfed3e9e18180
Anurag <anuragkumar7217@gmail.com>
2024-07-30 10:41:36 +0530

    modifications

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ export default function About(props) {
                 </p>
             </div>
             <div className="container mt-5">
-                <label for="formGroupExampleInput" className="form-label mx-1" style={{ color: props.mode === 'light' ? 'black' : 'white' }}><strong>Feedback : </strong></label>
+                <label htmlFor="formGroupExampleInput" className="form-label mx-1" style={{ color: props.mode === 'light' ? 'black' : 'white' }}><strong>Feedback : </strong></label>
                 <input type="text" style={{ width: '50%', border: `2px solid ${props.mode === 'light' ? 'black' : 'white'}`, backgroundColor: props.mode === 'light' ? 'white' : '#404040', color: props.mode === 'light' ? 'black' : 'white' }} id="feedback" placeholder="Please give your feedback." />
                 <br />
                 <input className="btn btn-primary" type="submit" value="Submit" />
